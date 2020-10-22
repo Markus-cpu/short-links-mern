c2bce01b729c861c82ae6bb682e72e82084094d7
Marat <markbass82@gmail.com>
2020-10-22 16:51:40 +0300

    'Авторизация пользователя, проверка данных при регистрации, и входе на сайт. Установка клиентской части приложения в папку client, materialize-css'

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'materialize-css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1><i className="material-icons">add</i>Hello, MERN-APP</h1>
+    </div>
+  );
+}
+
+export default App;
